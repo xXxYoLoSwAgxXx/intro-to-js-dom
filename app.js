eaa2295d898ca83ebbea7cd016d396409ae86e0d
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-03-13 08:28:57 -1000

    commit for the day

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("sanitycheck");
+console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+console.log(document.getElementsByTagName("div"));
+var tag = document.getElementsByTagName("div");
+console.log(tag);
