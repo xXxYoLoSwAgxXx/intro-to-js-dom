30f917c593617239c41b2f57517a25d8a4554d02
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-03-13 08:45:39 -1000

    commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-console.log("sanitycheck");
-console.log(document.getElementById("use-id-method"));
+// console.log("sanitycheck");
+// console.log(document.getElementById("use-id-method"));
 var idElement = document.getElementById("use-id-method");
 console.log(idElement);
-console.log(document.getElementsByTagName("div"));
+// console.log(document.getElementsByTagName("div"));
 var tag = document.getElementsByTagName("div");
 console.log(tag);
+
+var clas = document.getElementsByClassName("use-class-method");
+
+idElement.innerHTML = "i have been changed";
+console.log(idElement.innerHTML);
